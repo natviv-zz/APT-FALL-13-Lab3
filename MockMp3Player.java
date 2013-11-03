71ada07d421e57bcbd72abfda7faec7bd8bc3dda
VIVEK NATARAJAN <natviv@gmail.com>
2013-11-03 01:49:17 -0600

    added mock for Mock Mp3 Player

---
+++ MockMp3Player.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+ 
+ 
+public class MockMp3Player implements Mp3Player{
+ 
+    private ArrayList songs;
+    // Points to the current song 
+    private int csptr = 0;
+    // Boolean variable to see if song playing or not
+    private boolean isPlaying = false;
+    // Number of seconds played in the current song
+    private double songseconds = 0.0;
+ 
+    public MockMp3Player(){
+        this.songs = new ArrayList<String>();
+    }
+ 
+    @Override
+    public void play() {
+        if(this.songs.size() > 0){
+            this.isPlaying = true;
+            songseconds += 0.01;
+        }
+    }
+ 
+    @Override
+    public void pause() {
+        if(this.songs.size() > 0){
+            this.isPlaying = false;
+        }
+    }
+ 
+    @Override
+    public void stop() {
+        if(this.songs.size() > 0){
+            this.isPlaying = false;
+            this.songseconds = 0;
+        }
+    }
+ 
+    @Override
+    public double currentPosition() {
+        return this.songseconds;
+    }
+ 
+ 
+    @Override
+    public String currentSong() {
+        return String.valueOf(this.songs.get(csptr));
+    }
+ 
+    @Override
+    public void next() {
+        if(this.csptr < this.songs.size()) {
+            if(this.csptr < this.songs.size() - 1) {
+                this.csptr++;
+            }
+        }
+        this.songseconds = 0;
+ 
+    }
+ 
+    @Override
+    public void prev() {
+        if(this.csptr > 0) this.csptr--;
+        this.songseconds = 0;
+    }
+ 
+    @Override
+    public boolean isPlaying() {
+        return this.isPlaying;
+    }
+ 
+    @Override
+    public void loadSongs(ArrayList names) {
+        for(Object name : names){
+            this.songs.add(name);
+        }
+    }
+ 
+}
